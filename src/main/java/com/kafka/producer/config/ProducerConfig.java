777e9e4e9139a496f628de822e045b592053e4a4
Parth27 <diwanji.parth@gmail.com>
2021-04-27 22:21:46 -0400

    Update

--- src/main/java/com/kafka/producer/config/ProducerConfig.java
+++ src/main/java/com/kafka/producer/config/ProducerConfig.java
@@ -1,9 +1,9 @@
 package com.kafka.producer.config;
 
 public class ProducerConfig {
-    public static final String SERVERS = "3.137.149.32:9092,3.129.17.73:9092,3.133.148.122:9092";
+    public static final String SERVERS = "18.217.201.85:9092,3.128.95.84:9092,52.15.252.99:9092";
     public static final String TOPIC = "Messages";
     public static final int MEAN_BATCHSIZE = 10;
     public static final int PORT = 4567;
-    public static final long FREQUENCY = 10L;
+    public static final int FREQUENCY = 1;
 }
